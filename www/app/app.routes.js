d6dffba485772e6531c36dc9d0acefd087781d92
Bruno Silvano <bruno_705@hotmail.com>
2017-04-29 13:26:14 -0300

    main state setup

---
+++ www/app/app.routes.js
@@ -0,0 +1,22 @@
+(function () {
+  'use strict';
+
+  angular.module('app')
+    .config(routeConfig);
+
+  routeConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function routeConfig($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+
+      .state('app', {
+        url: '/',
+        templateUrl: 'app/properties/properties.html'
+      })
+
+    ;
+
+  };
+})();
