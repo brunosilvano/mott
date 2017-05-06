f9901455cf45d1fe8e8e5835a38da196dc7e2854
Bruno Costa <Bruno Costa>
2017-05-06 18:43:48 -0300

    improved error handling

--- www/app/properties/properties.js
+++ www/app/properties/properties.js
@@ -11,6 +11,7 @@
 
     vm.calculate = calculate;
     vm.decimalPlaces = config.getDecimalPlaces();
+    vm.error = false;
     vm.input = 1;
     vm.prop1 = null;
     vm.prop2 = null;
@@ -18,7 +19,6 @@
     vm.setInputType = setInputType;
     vm.substance = 1;
     vm.substanceList = [];
-
     vm.units = config.getUnits();
 
     activate();
@@ -32,47 +32,56 @@
     };
 
     function calculate() {
-      switch (vm.input) {
-        case 1:
-          var prop1 = converter.convert(vm.prop1, vm.units.p, 'MPa');
-          var prop2 = converter.convert(vm.prop2, vm.units.t, 'tempK');
-          vm.result = getWaterPop('PT', prop1, prop2);
-          convertOutput();
-          break;
-        case 2:
-          var prop1 = converter.convert(vm.prop1, vm.units.p, 'MPa');
-          var prop2 = converter.convert(vm.prop2, vm.units.h, 'kJ/kg');
-          vm.result = getWaterPop('PH', prop1, prop2);
-          convertOutput();
-          break;
-        case 3:
-          var prop1 = converter.convert(vm.prop1, vm.units.p, 'MPa');
-          var prop2 = converter.convert(vm.prop2, vm.units.s, 'kJ/kg degK');
-          vm.result = getWaterPop('PS', prop1, prop2);
-          convertOutput();
-          break;
-        case 4:
-          var prop1 = converter.convert(vm.prop1, vm.units.h, 'kJ/kg');
-          var prop2 = converter.convert(vm.prop2, vm.units.s, 'kJ/kg degK');
-          vm.result = getWaterPop('HS', prop1, prop2);
-          convertOutput();
-          break;
-        default:
-          break;
-      };
+      if (vm.prop1 && vm.prop2) {
+        switch (vm.input) {
+          case 1:
+            var prop1 = converter.convert(vm.prop1, vm.units.p, 'MPa');
+            var prop2 = converter.convert(vm.prop2, vm.units.t, 'tempK');
+            vm.result = getWaterPop('PT', prop1, prop2);
+            convertOutput();
+            break;
+          case 2:
+            var prop1 = converter.convert(vm.prop1, vm.units.p, 'MPa');
+            var prop2 = converter.convert(vm.prop2, vm.units.h, 'kJ/kg');
+            vm.result = getWaterPop('PH', prop1, prop2);
+            convertOutput();
+            break;
+          case 3:
+            var prop1 = converter.convert(vm.prop1, vm.units.p, 'MPa');
+            var prop2 = converter.convert(vm.prop2, vm.units.s, 'kJ/kg degK');
+            vm.result = getWaterPop('PS', prop1, prop2);
+            convertOutput();
+            break;
+          case 4:
+            var prop1 = converter.convert(vm.prop1, vm.units.h, 'kJ/kg');
+            var prop2 = converter.convert(vm.prop2, vm.units.s, 'kJ/kg degK');
+            vm.result = getWaterPop('HS', prop1, prop2);
+            convertOutput();
+            break;
+          default:
+            break;
+        };
+      }
     };
 
     function convertOutput() {
-      vm.result.t = converter.convert(vm.result.t, 'tempK', vm.units.t);
-      vm.result.p = converter.convert(vm.result.p, 'MPa', vm.units.p);
-      vm.result.rho = converter.convert(vm.result.rho, 'kg/m^3', vm.units.rho);
-      vm.result.v = converter.convert(vm.result.v, 'm^3/kg', vm.units.v);
-      vm.result.u = converter.convert(vm.result.u, 'kJ/kg', vm.units.u);
-      vm.result.s = converter.convert(vm.result.s, 'kJ/kg degK', vm.units.s);
-      vm.result.h = converter.convert(vm.result.h, 'kJ/kg', vm.units.h);
-      vm.result.cp = converter.convert(vm.result.cp, 'kJ/kg degK', vm.units.cp);
-      vm.result.cv = converter.convert(vm.result.cv, 'kJ/kg degK', vm.units.cv);
-      vm.result.w = converter.convert(vm.result.w, 'm/s', vm.units.w);
+      try {
+        vm.result.t = converter.convert(vm.result.t, 'tempK', vm.units.t);
+        vm.result.p = converter.convert(vm.result.p, 'MPa', vm.units.p);
+        vm.result.rho = converter.convert(vm.result.rho, 'kg/m^3', vm.units.rho);
+        vm.result.v = converter.convert(vm.result.v, 'm^3/kg', vm.units.v);
+        vm.result.u = converter.convert(vm.result.u, 'kJ/kg', vm.units.u);
+        vm.result.s = converter.convert(vm.result.s, 'kJ/kg degK', vm.units.s);
+        vm.result.h = converter.convert(vm.result.h, 'kJ/kg', vm.units.h);
+        vm.result.cp = converter.convert(vm.result.cp, 'kJ/kg degK', vm.units.cp);
+        vm.result.cv = converter.convert(vm.result.cv, 'kJ/kg degK', vm.units.cv);
+        vm.result.w = converter.convert(vm.result.w, 'm/s', vm.units.w);
+
+        vm.error = false;
+      }
+      catch (error) {
+        vm.error = true;
+      }
     };
 
     function getSubstances() {
